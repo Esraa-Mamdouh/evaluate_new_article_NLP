aae25fc06fffb71275de543be667a1a5bf6cc9df
Esraa-Mamdouh <esraa.ali9798@gmail.com>
2021-10-07 20:22:52 +0200

    feat: handle API request & update UI

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,28 +1,70 @@
+const fetch = require("node-fetch");
+
 function handleSubmit(event) {
     event.preventDefault()
 
+    // clear UI before every result
+    clearUI()
     // check what text was put into the form field
-    //check if enters valid URL
+    //TODO: check if enters valid URL
     let formText = document.getElementById('article-url').value
     //checkURL(formText)
 
+    //test logs
     console.log("::: Form Submitted :::")
     console.log("form text= ",formText)
-    // fetch('http://localhost:8081/add-url',{
-    //     method: 'POST',
-    //     credentials: 'same-origin',
-    //     mode:'cors',
-    //     dataType:'text',
-    //     headers: {
-    //         'Content-Type': 'application/json'
-    //       },
-    //       body: JSON.stringify({formText})
-    //     })
-    // .then(res => res.json())
-    // .then(function(res) {
-    //     CONSOLE.log("INSIDE RES", res.message)
-    //     document.getElementById('text').innerHTML = res.message
-    // })
+ 
+    // post req to the server to acess external UI 
+    postURL(formText)
+
 }
 
+// make a post request to the server to acess external API
+function postURL(url=" "){
+    fetch("http://localhost:8081/addURL", {
+        method: "POST",
+        mode: 'cors',
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({url})
+    }).then(res => {
+        console.log("inside formHandler")
+        console.log("res1= ",res)
+        res=res.json()
+        console.log("res2= ",res)
+        return res
+        
+    })
+    .then((res)=> {
+        console.log("inside then")
+        console.log("INSIDE RES", res)
+        updateUI(res)
+        
+    }).catch((rej)=>{
+        console.log("inside catch client")
+        console.log("rej= ",rej)
+    })
+}
+
+// update UI with new data we got from the external API
+function updateUI(res) {
+    document.getElementById('text').innerHTML = `Text: ${res.text}`
+    document.getElementById('agreement').innerHTML = `Agreement: ${res.agreement}`
+    document.getElementById('subjectivity').innerHTML = `Subjectivity: ${res.subjectivity}`
+    document.getElementById('confidence').innerHTML = `Confidence: ${res.confidence}`
+    document.getElementById('irony').innerHTML = `Irony: ${res.irony}`
+    document.getElementById('score_tag').innerHTML = `Score_tag: ${res.score_tag}`
+}
+
+//clear UI 
+function clearUI(){
+    document.getElementById('text').innerHTML = ` `
+    document.getElementById('agreement').innerHTML = ` `
+    document.getElementById('subjectivity').innerHTML = ` `
+    document.getElementById('confidence').innerHTML = ` `
+    document.getElementById('irony').innerHTML = ` `
+    document.getElementById('score_tag').innerHTML = ` `
+}
 export { handleSubmit }
--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,11 @@
 //Configure the dotenv environment variables
 require('dotenv').config()
-
+//const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const mockAPIResponse = require('./mockAPI.js')
 
 const PORT = 8081
 var path = require('path')
-
-// TODO add Configuration to be able to use env variables
-
 
 const axios = require('axios')
 
@@ -27,7 +25,8 @@ app.use(cors())
 
 //Configure express to use body-parser as middle-ware.
 const bodyParser = require('body-parser')
-app.use(express.urlencoded({ extended: false }))
+
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 //Configure express static directory.
@@ -38,36 +37,49 @@ app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
     res.sendFile(path.resolve('src/client/views/index.html'))
 })
-// a route that handling post request for new URL that coming from the frontend
-// app.post('/addURL', async(req, res)=>{
-//     console.log("inside server")
-//     const inputURL =req.body.url; 
-//     const fetchedURL = `${API_URL}?key=${API_KEY}&url=${inputURL}&lang=en`;
-//     try{
-//         //TODO: باقي اللوجيك
-//         const b =await axios(fetchedURL)
-//     }
-//     catch(e){
-//         console.log("error= ",e)
-//     }
-// })
-/* TODO:
-    1. GET the url from the request body
-    2. Build the URL it should be something like `${BASE_API_URL}?key=${MEAN_CLOUD_API_KEY}&url=${req.body.url}&lang=en`
-    3. Fetch Data from API
-    4. Send it to the client
-    5. REMOVE THIS TODO AFTER DOING IT 😎😎
-    server sends only specified data to the client with below codes
-     const sample = {
-       text: '',
-       score_tag : '',
-       agreement : '',
-       subjectivity : '',
-       confidence : '',
-       irony : ''
-     }
-*/
 
+// a route that handling post request for new URL that coming from the frontend
+app.post('/addURL',async (req, res) => {
+    //1. Get the submitted url from the fetch req body
+    const  articleUrl  = req.body.url
+     //test logs
+    console.log("request____________________= ",req)
+    console.log("a111111111111rticle url= ") 
+    console.log(articleUrl)
+
+    //2. create the url we will use to access the external API
+    const  APIURL = `${API_URL}?key=${API_KEY}&url=${articleUrl}&lang=en`
+    //test logs
+        console.log("heeeeereeeee33333333333")
+        console.log("meaningCloudUrl= ", APIURL)
+
+    //3. fetch the external API 
+    const {data} = await axios.get(APIURL)
+     //test logs
+    //const datanew= await JSON.parse(JSON.stringify(data));
+    //const newdata= data.json()
+    console.log("dataaaaaaaaaa= ", data)
+    //console.log("dataaaaaaaaaa22222222222= ", datanew)
+
+      try {
+        //4. send data fetched Client 
+        //test logs
+        console.log("data= 1" ,data.sentence_list[0].text," 2  ",data.score_tag,"  3 ",data.agreement,"  4  ",data.subjectivity,"  5  ",data.confidence,"  6  ",data.irony )
+        res.send({
+        text: data.sentence_list[0].text || '',
+        score_tag: data.score_tag,
+        agreement: data.agreement,
+        subjectivity: data.subjectivity,
+        confidence: data.confidence,
+        irony: data.irony,
+      })
+    } catch (error) {
+        //5. if error occures while sending catch the error ad log it to console
+         //test logs
+      console.log("error= ",error.message)
+    }
+  })
+  
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
     console.log("test")
@@ -79,4 +91,6 @@ app.listen(PORT, (error) => {
     console.log(`Server listening on port ${PORT}!`)
 })
 
-// TODO: export app to use it in the unit testing
+//TODO: export app to use it in the unit testing
+//TODO: add clear UI  before the next article test or show loading 
+
